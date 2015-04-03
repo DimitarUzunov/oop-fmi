3c67e8abaf56542274f0e8f740f5e9a57dc325fa
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-04-03 20:38:24 +0300

    added new tests for the big four

--- 10-classes/main.cpp
+++ 10-classes/main.cpp
@@ -4,52 +4,99 @@
 #include "person.h"
 #include "song.h"
 #include "album.h"
+#include "playlist.h"
+#include "polygon.h"
 #include "dog.h"
 #include "train.h"
 #include "book.h"
 #include "circle.h"
 
 void testGuitar() {
-	char electric[] = "Electric";
-	Guitar guitar(electric, 6, 22);
+	Guitar acoustic;
+	Guitar electric = acoustic;
 
-	std::cout << guitar.getType() << '\n';
-	guitar.setType("Acoustic");
-	std::cout << guitar.getType() << '\n';
-	std::cout << guitar.getStrings() << '\n';
-	std::cout << guitar.getFrets() << '\n';
+	electric.setType("Electric");
+	electric.setFrets(22);
+
+	std::cout << electric.getType() << '\n';
+	std::cout << acoustic.getType() << '\n';
+
+	electric = acoustic;
+
+	std::cout << electric.getType() << '\n';
 }
 
 void testPerson() {
-	Person person;
-	std::cout << person.getFirstName() << '\n';
+	Person maria;
 
-	Person maria("Maria", "Penkova", 19, 'f');
+	maria = Person("Maria", "Penkova", 19, 'f');
+
+	std::cout << maria.getFirstName() << '\n';
 	std::cout << maria.getLastName() << '\n';
 }
 
 void testSong() {
-	Person artist("Slash", "Hudson", 49, 'm');
-	Song song("Bent to fly", artist, 297);
+	Song unknown;
+	Song song("Bent to fly", "Slash", 297);
+
+	unknown = song;
+	unknown.setName("Too far gone");
+	unknown.setDuration(247);
 
 	std::cout << song.getName() << '\n';
-	std::cout << song.getArtist()->getFirstName() << '\n';
+	std::cout << song.getArtist() << '\n';
+	std::cout << unknown.getName() << '\n';
+	std::cout << unknown.getArtist() << '\n';
 }
 
 void testAlbum() {
-	Person artist("Slash", "Hudson", 49, 'm');
-	Album album("World on fire", artist, 17);
+	Album album("World on fire", "Slash", 17);
 
 	std::cout << album.getName() << '\n';
-	std::cout << album.getArtist()->getFirstName() << '\n';
+	std::cout << album.getArtist() << '\n';
 	std::cout << album.getSongsCount() << '\n';
 
-	album.setName("Apocalyptic love");
-	album.setSongsCount(15);
+	Album apocalyptic = album;
+	apocalyptic.setName("Apocalyptic love");
+	apocalyptic.setSongsCount(15);
 
 	std::cout << album.getName() << '\n';
-	std::cout << album.getArtist()->getFirstName() << '\n';
-	std::cout << album.getSongsCount() << '\n';
+	std::cout << apocalyptic.getName() << '\n';
+	std::cout << apocalyptic.getArtist() << '\n';
+	std::cout << apocalyptic.getSongsCount() << '\n';
+}
+
+void testPlaylist() {
+	Playlist playlist;
+	playlist.addSong(Song());
+	Playlist rockNRoll = playlist;
+
+	rockNRoll.setName("Rock 'n' roll");
+	rockNRoll.addSong(Song("Bent to fly", "Slash", 297));
+	rockNRoll.addSong(Song("Too far gone", "Slash", 247));
+
+	std::cout << playlist.getName() << '\n';
+	std::cout << rockNRoll.getName() << '\n';
+	for (int i = 0; i < rockNRoll.getSongsCount(); i++) {
+		std::cout << rockNRoll.getSongById(i)->getName() << '\n';
+	}
+
+	Playlist metal;
+	metal = rockNRoll;
+	metal.setName("Metal");
+	metal.addSong(Song("Nothing to say", "Slash", 326));
+
+	for (int i = 0; i < metal.getSongsCount(); i++) {
+		std::cout << metal.getSongById(i)->getName() << '\n';
+	}
+}
+
+void testPolygon() {
+	Polygon triangle;
+	triangle.setSideLength(10);
+
+	std::cout << "Perimeter: " << triangle.getPerimeter() << '\n';
+	std::cout << "Area: " << triangle.getArea() << '\n';
 }
 
 void testDog() {
@@ -72,10 +119,9 @@ void testTrain() {
 }
 
 void testBook() {
-	Person author("Robin", "Sharma", 49, 'm');
-	Book book("The leader who had no title", author, 300);
+	Book book("The leader who had no title", "Robin Sharma", 203);
 
-	std::cout << '\'' << book.getName() << "' by " << book.getAuthor()->getFirstName() << ' ' << book.getAuthor()->getLastName() << '\n';
+	std::cout << '\'' << book.getName() << "' by " << book.getAuthor() << '\n';
 }
 
 void testCircle() {
@@ -91,6 +137,8 @@ int main() {
 	// testPerson();
 	// testSong();
 	// testAlbum();
+	// testPlaylist();
+	// testPolygon();
 	// testDog();
 	// testTrain();
 	// testBook();
